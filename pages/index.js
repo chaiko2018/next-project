0dde6da3fd99a3a23f049f350a9707c5cbcccc09
tommy <afitamoon@gmail.com>
2020-04-12 14:34:38 +0900

    change Button

--- pages/index.js
+++ pages/index.js
@@ -58,6 +58,8 @@ import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import Layout from "../components/layout";
+import Input from "@material-ui/core/Input";
+import { FormControl, InputLabel } from "@material-ui/core";
 
 const App = () => {
   const [todos, setTodos] = useState([]);
@@ -82,16 +84,32 @@ const App = () => {
   return (
     <Layout>
       <h1>Todo App</h1>
-      <input
-        type="text"
-        name="todo"
-        onChange={(e) => setTmpTodo(e.target.value)}
-        value={tmpTodo}
-      />
-      <Button variant="outlined" color="primary" size="small" onClick={addTodo}>
-        Add Todo
-      </Button>
-      <button onClick={() => addTodo()}>Add</button>
+      <FormControl>
+        <Input
+          type="text"
+          id="standard-basic"
+          label="Stardard"
+          name="todo"
+          onChange={(e) => setTmpTodo(e.target.value)}
+          value={tmpTodo}
+        />
+        <Button
+          variant="outlined"
+          color="primary"
+          size="small"
+          onClick={addTodo}
+        >
+          Add Todo
+        </Button>
+        <Button
+          color="primary"
+          variant="contained"
+          size="small"
+          onClick={() => addTodo()}
+        >
+          Add Todo
+        </Button>
+      </FormControl>
       <ul>
         {todos.map((todo, index) => {
           return (
